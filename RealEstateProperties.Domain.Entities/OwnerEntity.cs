50f9649b8e6851739e9de45219f9f0540fe1a4cd
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-01-29 20:28:52 -0500

    :recycle: Record to add the photo in owner entity

--- RealEstateProperties.Domain.Entities/OwnerEntity.cs
+++ RealEstateProperties.Domain.Entities/OwnerEntity.cs
@@ -5,10 +5,12 @@ namespace RealEstateProperties.Domain.Entities
     public Guid OwnerId { get; set; }
     public required string Name { get; set; }
     public required string Address { get; set; }
-    public byte[]? Photo { get; set; }
+    public OwnerPhoto? Photo { get; set; }
     public DateTimeOffset Birthday { get; set; }
     public DateTimeOffset Created { get; set; }
     public byte[] Version { get; set; } = null!;
     public ICollection<PropertyEntity> Properties { get; set; } = [];
   }
+
+  public record OwnerPhoto(byte[] Photo, string PhotoName);
 }
