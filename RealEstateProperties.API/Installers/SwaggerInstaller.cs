c98e4832ec27d1875b27f0b224c886a0cb185cf5
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-03-07 03:10:16 -0500

    :truck: ApiConfigKeys enum is moved as utility

--- RealEstateProperties.API/Installers/SwaggerInstaller.cs
+++ RealEstateProperties.API/Installers/SwaggerInstaller.cs
@@ -1,7 +1,7 @@
 using Microsoft.OpenApi.Models;
 using RealEstateProperties.API.Filters;
 using RealEstateProperties.API.Options;
-using RealEstateProperties.Domain.Helpers;
+using RealEstateProperties.API.Utils;
 
 namespace RealEstateProperties.API.Installers;
 
