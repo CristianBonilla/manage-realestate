48e53acfbaa7e93a4d516e1745a0635af53c7e9d
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-08-30 18:45:15 -0500

    :recycle: Adjusted additional API config keys

--- RealEstateProperties.API/Utils/ApiConfigKeys.cs
+++ RealEstateProperties.API/Utils/ApiConfigKeys.cs
@@ -4,5 +4,8 @@ record struct ApiConfigKeys
 {
   public const string AllowOrigins = nameof(AllowOrigins);
   public const string Bearer = nameof(Bearer);
-  public const string RealEstatePropertiesConnection = nameof(RealEstatePropertiesConnection);
+  public const string DefaultDbConnection = nameof(DefaultDbConnection);
+  public const string LocalDbConnection = nameof(LocalDbConnection);
+
+  public static bool IsLocalDbPlatform => bool.Parse(Environment.GetEnvironmentVariable("LOCALDB_PLATFORM") ?? "false");
 }
